3d7c7cce4c54456f4e10a378b96198ab842391c4
AlbinDeploy <albinpremam12@gmail.com>
2026-03-22 21:37:09 -0700

    Implement HTTP to HTTPS redirection
    Added HTTP server to redirect traffic to HTTPS.

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const { default: consola } = require('consola');
 // Check if -dev flag is passed
 const isDev = process.argv.includes('--dev');
 const dev = isDev;
+
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
@@ -22,12 +23,28 @@ const httpsOptions = {
 
 app.prepare().then(() => {
 
-  // Create HTTPS server
+  const HTTPS_PORT = dev ? 3000 : 443;
+  const HTTP_PORT = dev ? 3001 : 80;
+
+  // HTTPS server (main app)
   createServer(httpsOptions, (req, res) => {
     const parsedUrl = parse(req.url, true);
     handle(req, res, parsedUrl);
-  }).listen((dev ? 3000 : 3000), (err) => {
+  }).listen(HTTPS_PORT, (err) => {
     if (err) throw err;
-    console.log(`> Ready on https://localhost${dev ? ':3000' : ':3000'} (${dev ? 'development' : 'production'} mode)`);
+    console.log(`> HTTPS Ready on https://localhost:${HTTPS_PORT}`);
   });
+
+  // HTTP server (redirect to HTTPS)
+  http.createServer((req, res) => {
+    const host = req.headers['host'] || '';
+    const redirectURL = `https://${host}${req.url}`;
+    
+    res.writeHead(301, { Location: redirectURL });
+    res.end();
+  }).listen(HTTP_PORT, (err) => {
+    if (err) throw err;
+    console.log(`> HTTP Redirect running on http://localhost:${HTTP_PORT}`);
+  });
+
 });
